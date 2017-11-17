1a7316fa036bc5484b86c480164fff826d4917a1
sumitkeswani123@gmail.com <skeswani6@minsky4.local.lan>
2017-11-17 18:01:37 -0500

    loading pre-trained models and creating model

---
+++ model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.init as init
+from torch.autograd import Variable
+import VQA_model
+from caption_model import EncoderCNN, DecoderRNN
+
+class VQA_Cap(nn.Module):
+	super(VQA_Cap, self).__init__()
+	
+	# load VQA model
+	log = torch.load('VQA_model/2017-08-04_00.55.19.pth')
+	tokens = len(log['vocab']['question']) + 1
+	net = torch.nn.DataParallel(VQA_model.Net(tokens))
+	net.load_state_dict(log['weights'])
+	net.module.classifier._modules['lin2'] = nn.Linear(1024, 512)	#replaced last linear layer!
+	self.VQAmodel = net
+
+	# load image captioning model
+	encoder = EncoderCNN(256)
+	decoder = DecoderRNN(args.embed_size, args.hidden_size, 
+                         len(vocab), args.num_layers)
+
+	
+
