a10b70fe9d75947b4a23909c462bb1d7df46bf57
sumitkeswani123@gmail.com <skeswani6@minsky4.local.lan>
2017-11-22 16:17:32 -0500

    completed VQA dataparallel model

--- model.py
+++ model.py
@@ -7,20 +7,33 @@ import VQA_model
 from caption_model import EncoderCNN, DecoderRNN
 
 class VQA_Cap(nn.Module):
-	super(VQA_Cap, self).__init__()
-	
-	# load VQA model
-	log = torch.load('VQA_model/2017-08-04_00.55.19.pth')
-	tokens = len(log['vocab']['question']) + 1
-	net = torch.nn.DataParallel(VQA_model.Net(tokens))
-	net.load_state_dict(log['weights'])
-	net.module.classifier._modules['lin2'] = nn.Linear(1024, 512)	#replaced last linear layer!
-	self.VQAmodel = net
+	def __init__(self):
+		super(VQA_Cap, self).__init__()
+		
+		# load VQA model
+		log = torch.load('VQA_model/2017-08-04_00.55.19.pth')
+		tokens = len(log['vocab']['question']) + 1
+		net = torch.nn.DataParallel(VQA_model.Net(tokens))
+		net.load_state_dict(log['weights'])
+			
+		# set require_grad to false
+		for param in net.parameters():	
+			param.requires_grad = False
 
-	# load image captioning model
-	encoder = EncoderCNN(256)
-	decoder = DecoderRNN(args.embed_size, args.hidden_size, 
-                         len(vocab), args.num_layers)
+		# Remove last layer and add a different layer
+		net.module.classifier._modules['lin2'] = nn.Linear(1024, 512)	#replaced last linear layer!
+		self.VQAmodel = net
 
-	
+		# load image captioning model
+		encoder = EncoderCNN(256)
+		decoder = DecoderRNN(args.embed_size, args.hidden_size, len(vocab), args.num_layers)
+
+		# Load the trained model parameters
+		encoder.load_state_dict(torch.load(args.encoder_path)).cuda()
+		decoder.load_state_dict(torch.load(args.decoder_path)).cuda()
+
+	def forward():
+
+
+
 
